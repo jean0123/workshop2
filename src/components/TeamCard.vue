02e03ce7e4ff342b3a26692d9a478a82c0c1b309
unknown <jean93pierre06@gmail.com>
2020-12-06 20:40:41 -0500

    Proyecto No. 2 mintic

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card text-white" style="background-color: #4e5459">
+    <div class="d-flex justify-content-center p-2">
+      <img :src="member.imagen" alt="imagen del integrante" />
+    </div>
+    <div class="card-body" style="height: 230px">
+      <h6 class="card-title">Código: {{ member.codigo }}</h6>
+      <h6 class="card-title">Rol: {{ member.rol }}</h6>
+      <hr style="background-color: white" />
+      <h5 class="card-text">{{ member.nombre }}</h5>
+      <p class="card-text">{{ member.descripcion }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamCard",
+  props: {
+    member: Object,
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg sticky-top navbar-dark"
+    style="background-color: #4e5459"
+  >
+    <a class="navbar-brand" href="# ">
+      <img
+        src="../assets/logo.jpg"
+        width="150"
+        height="70"
+        class="d-inline-block align-top"
+        style="margin: -10px"
+      />
+    </a>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav ">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="# "
+            >Home <span class="sr-only">(current)</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="# ">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="# ">News</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="# ">Team</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TopMenu",
+};
+</script>
